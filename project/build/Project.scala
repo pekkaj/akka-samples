f57376ae8c420f40b3e12151cfdcaba7b0868bbd
Erick Fleming <efleming969@gmail.com>
2010-04-06 09:48:57 -0500

    upgraded to 0.8.1 and added hack for redisclient dep

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -18,8 +18,13 @@ class Project(info: ProjectInfo) extends DefaultWebProject(info) {
 		"org.eclipse.jetty"  % "jetty-server"   % "7.0.1.v20091125" % "test",
 		"org.eclipse.jetty"  % "jetty-webapp"   % "7.0.1.v20091125" % "test",
 
+		/* redis client dependecy */
+		"com.redis" % "redisclient" % "2.8.0.Beta1-1.3-SNAPSHOT" % "compile" from (
+			"http://github.com/jboner/akka/blob/master/embedded-repo/com/redis/redisclient/2.8.0.Beta1-1.3-SNAPSHOT/redisclient-2.8.0.Beta1-1.3-SNAPSHOT.jar"),
+
 		/* akka dependencies */
-		"se.scalablesolutions.akka" % "akka-kernel_2.8.0.Beta1"  % "0.8" % "compile",
-		"se.scalablesolutions.akka" % "akka-core_2.8.0.Beta1"    % "0.8" % "compile",
-		"se.scalablesolutions.akka" % "akka-rest_2.8.0.Beta1"    % "0.8" % "compile")
+		"se.scalablesolutions.akka" % "akka-kernel_2.8.0.Beta1"  % "0.8.1" % "compile",
+		"se.scalablesolutions.akka" % "akka-core_2.8.0.Beta1"    % "0.8.1" % "compile",
+		"se.scalablesolutions.akka" % "akka-servlet_2.8.0.Beta1" % "0.8.1" % "compile",
+		"se.scalablesolutions.akka" % "akka-rest_2.8.0.Beta1"    % "0.8.1" % "compile")
 }
